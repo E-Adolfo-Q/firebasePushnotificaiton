082a1f5ef2dbca0dc374b3653c1830edddb5606a
Esteban Lagos <elagosquintanilla@gmail.com>
2019-08-11 14:22:12 -0400

    Firebase push notificación en android con sonido

---
+++ App.js
@@ -0,0 +1,150 @@
+import React, {Component} from 'react';
+import { Platform, StyleSheet, Text, View, Alert, AsyncStorage} from 'react-native';
+import firebase from 'react-native-firebase';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+export default class App extends Component{
+
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners(); //add this line
+  }
+
+  componentWillUnmount() {
+    this.notificationListener;
+    this.notificationOpenedListener;
+  }
+
+  //1
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  //3
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        //usuario tiene un token de dispositivo
+        console.log('fcmToken:', fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+    console.log('fcmToken:', fcmToken);
+  }
+
+  //2
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // Usuario tiene autorización 
+      this.getToken();
+    } catch (error) {
+      // Usuario no tiene permisos de autorización 
+      console.log('permission rejected');
+    }
+  }
+
+  async createNotificationListeners() {
+    /*
+    *Se activa cuando se recibe una notificación particular en primer plano
+    * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      console.log('onNotification:');
+
+        const localNotification = new firebase.notifications.Notification({
+          sound: 'sampleaudio',
+          show_in_foreground: true,
+        })
+        .setSound('sampleaudio.wav')
+        .setNotificationId(notification.notificationId)
+        .setTitle(notification.title)
+        .setBody(notification.body)
+        .android.setChannelId('fcm_FirebaseNotifiction_default_channel')
+        .android.setSmallIcon('@drawable/ic_launcher') //crear este icono en Android Studio
+        .android.setColor('#000000') // puedes establecer un color aquí
+        .android.setPriority(firebase.notifications.Android.Priority.High);
+
+        firebase.notifications()
+          .displayNotification(localNotification)
+          .catch(err => console.error(err));
+    });
+
+    const channel = new firebase.notifications.Android.Channel('fcm_FirebaseNotifiction_default_channel', 'Demo app name', firebase.notifications.Android.Importance.High)
+      .setDescription('Demo app description')
+      .setSound('sampleaudio.wav');
+    firebase.notifications().android.createChannel(channel);
+
+    /*
+    * 
+      Si su aplicación está en segundo plano, puede escuchar cuando se hace clic / se toca / abre una notificación de la siguiente manera:
+    * */
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      console.log('onNotificationOpened:');
+      Alert.alert(title, body)
+    });
+
+    /*
+    * 
+      Si su App está cerrada, puede verificar si se abrió al hacer clic / tocar / abrir una notificación de la siguiente manera:
+    * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      console.log('getInitialNotification:');
+      Alert.alert(title, body)
+    }
+    /*
+    * Activado para datos solo carga útil en primer plano
+    * */
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      //process data message
+      console.log("JSON.stringify:", JSON.stringify(message));
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
